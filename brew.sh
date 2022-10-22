21b727dc4d4eb6723822bb073679d599269d5a65
Atsunori Saito <sai@yedo.com>
2022-10-22 15:12:25 +0900

    Updated.

---
+++ brew.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+PATH=/opt/homebrew/bin:$PATH
+brew tap homebrew/cask-fonts
+brew install        bat
+brew install --cask docker
+brew install        exa
+brew install        fd
+brew install --cask fig
+brew install        figlet
+brew install        fish
+brew install --cask font-fira-code-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
+brew install        fzf
+brew install        gh
+brew install        ghq
+brew install --cask gitkraken
+brew install        gnupg
+brew install --cask google-chrome
+brew install        hexyl
+brew install --cask jetbrains-toolbox
+brew install --cask mimestream
+brew install        neovim
+brew install        nodenv
+brew install        pinentry-mac
+brew install        pyenv
+brew install        ripgrep
+brew install --cask slack
+brew install        starship
+brew install        the_silver_searcher
+brew install        tig
+brew install --cask zoom
+brew install        zoxide
